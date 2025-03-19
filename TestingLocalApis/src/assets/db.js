e20ec7e43ffe4f1508502640c577231fc8cf89d9
davidojo1144 <davco1144@gmail.com>
2025-03-19 05:11:04 +0100

    Testing and sending authors apis to the server backend side

--- TestingLocalApis/src/assets/db.js
+++ TestingLocalApis/src/assets/db.js
@@ -1,28 +1,39 @@
+import rowling from "./rowling.jpeg"
+import adichie from "./adichie.jpeg"
+import stephen from "./Stephen_King.jpg"
+import margaret from "./Margaret-Atwood.webp"
+import george from "./George-RR-Martin.webp"
+
+
+
+
+
+
 export const authors = [
     [
         {
           "name": "J.K. Rowling",
-          "picture": "https://example.com/jk-rowling.jpg",
+          "picture": [rowling],
           "age": 58
         },
         {
           "name": "Stephen King",
-          "picture": "https://example.com/stephen-king.jpg",
+          "picture": [stephen],
           "age": 76
         },
         {
           "name": "Margaret Atwood",
-          "picture": "https://example.com/margaret-atwood.jpg",
+          "picture": [margaret],
           "age": 84
         },
         {
           "name": "George R.R. Martin",
-          "picture": "https://example.com/george-martin.jpg",
+          "picture": [george],
           "age": 75
         },
         {
           "name": "Chimamanda Ngozi Adichie",
-          "picture": "https://example.com/chimamanda-adichie.jpg",
+          "picture": [adichie],
           "age": 46
         }
       ]
