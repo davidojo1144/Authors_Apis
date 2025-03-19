6f8940995359ac6778c2e20b826e3f7f055c3028
davidojo1144 <davco1144@gmail.com>
2025-03-19 04:41:49 +0100

    Testing and sending authors apis to the server backend side

---
+++ TestingLocalApis/src/assets/assets.js
@@ -0,0 +1,29 @@
+export const authors = [
+    [
+        {
+          "name": "J.K. Rowling",
+          "picture": "https://example.com/jk-rowling.jpg",
+          "age": 58
+        },
+        {
+          "name": "Stephen King",
+          "picture": "https://example.com/stephen-king.jpg",
+          "age": 76
+        },
+        {
+          "name": "Margaret Atwood",
+          "picture": "https://example.com/margaret-atwood.jpg",
+          "age": 84
+        },
+        {
+          "name": "George R.R. Martin",
+          "picture": "https://example.com/george-martin.jpg",
+          "age": 75
+        },
+        {
+          "name": "Chimamanda Ngozi Adichie",
+          "picture": "https://example.com/chimamanda-adichie.jpg",
+          "age": 46
+        }
+      ]
+]
